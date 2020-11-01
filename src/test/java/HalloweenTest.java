3c5ad7da4677d9d942c4d5833d5bae605ba73f6c
RajmiNakarmi <68827515+RajmiNakarmi@users.noreply.github.com>
2020-11-01 00:34:14 -0400

    enjoy your halloween at home commit

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -2,5 +2,10 @@ public class HalloweenTest {
     public static void main(String[] args) {
         System.out.println("Trick or Treat");
         // No Trick or Treat due to covid-19
+        //Lets not do Trick or Treak
+        // Be Safe,stay Healthy
+        //just buy your favorit candy and eat.
+        //have fun inside the house.
+
     }
 }
